71d4decdd6ccd7fb66c3afde0fa847148002807a
TheNCf <nikitadorofeevandreevich2000@gmail.com>
2026-07-18 23:36:10 +0300

    Changes
     - Progress on enemy spawner.
     - Updated Visual Studio package

---
+++ Assets/Game/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour, IPoolableObject
+{
+    public void Activate()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void ResetObject()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Game/Scripts/Spawners/EnemySpawner.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : SpawnerBase<Enemy>
+{
+    [SerializeField] private List<Transform> _spawnPoints;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
