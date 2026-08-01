b07c7456ce4fe57d46b6ee995cb36aa5c5eb35f3
TheNCf <nikitadorofeevandreevich2000@gmail.com>
2026-08-02 00:07:38 +0300

    Update
     - Added Targeter component to player
     - Updated Enemy prefab

---
+++ Assets/Game/Scripts/Enemy/Targeter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Targeter : MonoBehaviour, ITargetable
+{
+    public Vector3 Position => transform.position;
+    public Vector3 Velocity { get; private set; }
+
+    private Vector3 _lastPosition;
+
+    private void Start()
+    {
+        _lastPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        Velocity = (transform.position - _lastPosition) / Time.deltaTime;
+        _lastPosition = transform.position;
+    }
+}
